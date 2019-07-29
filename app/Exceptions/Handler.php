39b53e5568c0f73dfc331439b303c24eb02403cb
Your Name <you@example.com>
2019-07-29 21:48:09 +0300

    second

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,8 +47,8 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
 
-        //return parent::render($request, $exception);
+        return parent::render($request, $exception);
         //return view('errors.404.blade');
-        return response ('404');
+        //return response ('404');
     }
 }
